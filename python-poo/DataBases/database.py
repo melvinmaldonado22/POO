8fd2fc7a24549e2105bf9b213353fa04d24ae401
melvinmaldonado22 <xmelvinymaldonadozmoncada@gmail.com>
2024-11-01 01:13:16 -0600

    conection string data

--- python-poo/DataBases/database.py
+++ python-poo/DataBases/database.py
@@ -1,7 +1,7 @@
 import pymysql
 
 class Database:
-    def __init__(self, host='localhost', user='root', password='', db='registro'):
+    def __init__(self, host='bv1bgx6yniwakcmhh1w2-mysql.services.clever-cloud.com', user='uglgrub1myyug9eq', password='JAubBmKBcm46lVKnayd0', db='bv1bgx6yniwakcmhh1w2'):
         self.connection = pymysql.connect(
             host=host,
             user=user,
